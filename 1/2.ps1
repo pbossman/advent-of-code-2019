0ac4e956421119f75a9843caac23bdea297129fd
PBossman <12040309+pbossman@users.noreply.github.com>
2019-12-01 20:18:14 -0500

    add verbose, to show recusive adds

--- 1/2.ps1
+++ 1/2.ps1
@@ -3,19 +3,25 @@
 # https://adventofcode.com/2019/day/1
 
 function get-fuel {
+    [CmdletBinding()]
     param(
         # Parameter help description
         [Parameter(Mandatory=$true)]
         [int] $Mass
     )
 
-    Write-output ([math]::floor($mass / 3) - 2)
+    $var = ([math]::floor($mass / 3) - 2)
+    Write-Verbose $var
+    if ($var -gt 0) {
+        $var + (get-fuel -Mass $var)
+        Write-Verbose "Addition $var"
+    }
 }
 
-$input1 = Get-Content -Path ./input1.txt
+$input2 = Get-Content -Path ./input2.txt
 
 $result = 0
-$result = $input1 | %{
+$result = $input2 | %{
     (get-fuel -Mass $_)    
 } | Measure-Object -Sum  | Select-Object -ExpandProperty Sum 
 
